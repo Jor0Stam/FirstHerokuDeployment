737fb65c9516670630c0386781f1f2fc5b4ec97f
Jor0 <g.a.stamenov@gmail.com>
2017-05-24 13:38:35 +0300

    Fix

--- website/settings.py
+++ website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'blog',
-            'USER': 'myprojectuser',
+            'USER': 'hack',
             'PASSWORD': 'parola',
             'HOST': 'localhost',
             'PORT': '',
