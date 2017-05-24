4e11a44dbc0413ede59f12fd8cc1f1be7d4df246
Jor0 <g.a.stamenov@gmail.com>
2017-05-24 12:59:28 +0300

    Fixing Postgres user

--- website/settings.py
+++ website/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'blog',
             'USER': 'myprojectuser',
-            'PASSWORD': 'password',
+            'PASSWORD': '',
             'HOST': 'localhost',
             'PORT': '',
     }
